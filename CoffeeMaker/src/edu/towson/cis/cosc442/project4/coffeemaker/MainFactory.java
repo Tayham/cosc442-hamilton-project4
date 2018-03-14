400440bd2ed7f97b42990adc98143ed1be9ee06f
Taylor <tayham1996@gmail.com>
2018-03-14 18:54:34 -0400

    Task 4 – Acceptance Testing Completed

---
+++ CoffeeMaker/src/edu/towson/cis/cosc442/project4/coffeemaker/MainFactory.java
@@ -0,0 +1,31 @@
+package edu.towson.cis.cosc442.project4.coffeemaker;
+
+
+
+/**
+ * The class <code>MainFactory</code> implements static methods that return instances of the class <code>{@link Main}</code>.
+ *
+ * @generatedBy CodePro at 3/14/18 5:38 PM
+ * @author Taylor
+ * @version $Revision: 1.0 $
+ */
+public class MainFactory
+ {
+	/**
+	 * Prevent creation of instances of this class.
+	 *
+	 * @generatedBy CodePro at 3/14/18 5:38 PM
+	 */
+	private MainFactory() {
+	}
+
+
+	/**
+	 * Create an instance of the class <code>{@link Main}</code>.
+	 *
+	 * @generatedBy CodePro at 3/14/18 5:38 PM
+	 */
+	public static Main createMain() {
+		return new Main();
+	}
+}
